123632b6a790f0e00a5dc2f3b28cb2afb25ad74d
Eugene Kulyabin <152572717+unsigable@users.noreply.github.com>
2024-09-03 21:40:03 +0600

    Add files via upload

---
+++ src/WhacAMole.java
@@ -0,0 +1,128 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+import javax.swing.*;
+
+public class WhacAMole {
+    int boardWidth = 600;
+    int boardHeight = 650;  //50px for the text panel on top
+
+    JFrame frame = new JFrame("Mario: Whac A Mole");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel(); 
+    JPanel boardPanel = new JPanel();
+
+    JButton[] board = new JButton[9];
+    ImageIcon moleIcon;
+    ImageIcon plantIcon;
+
+    JButton currMoleTile;
+    JButton currPlantTile;
+
+    Random random = new Random();
+    Timer setMoleTimer;
+    Timer setPlantTimer;
+    int score = 0;
+
+    WhacAMole() {
+        // frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Score: 0");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(3, 3)); 
+        // boardPanel.setBackground(Color.black);
+        frame.add(boardPanel);
+
+        // plantIcon = new ImageIcon(getClass().getResource("./piranha.png"));
+        Image plantImg = new ImageIcon(getClass().getResource("./piranha.png")).getImage();
+        plantIcon = new ImageIcon(plantImg.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH));
+
+        Image moleImg = new ImageIcon(getClass().getResource("./monty.png")).getImage();
+        moleIcon = new ImageIcon(moleImg.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH));
+
+        score = 0;
+
+        for (int i = 0; i < 9; i++) {
+            JButton tile = new JButton();
+            board[i] = tile;
+            boardPanel.add(tile);
+            tile.setFocusable(false);
+            // tile.setIcon(plantIcon);
+            // tile.setIcon(moleIcon);
+            tile.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    JButton tile = (JButton) e.getSource();
+                    if (tile == currMoleTile) {
+                        score += 10;
+                        textLabel.setText("Score: " + Integer.toString(score));
+                    }
+                    else if (tile == currPlantTile) {
+                        textLabel.setText("Game Over: " + Integer.toString(score));
+                        setMoleTimer.stop();
+                        setPlantTimer.stop();
+                        for (int i = 0; i < 9; i++) {
+                            board[i].setEnabled(false);
+                        }
+                    }
+                }
+            });
+        }
+
+        setMoleTimer = new Timer(1000, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //remove mole from current tile
+                if (currMoleTile != null) {
+                    currMoleTile.setIcon(null);
+                    currMoleTile = null;
+                }
+
+                //randomly select another tile
+                int num = random.nextInt(9); //0-8
+                JButton tile = board[num];
+
+                //if tile is occupied by plant,skip tile for this turn
+                if (currPlantTile == tile) return;
+
+                //set tile to mole
+                currMoleTile = tile;
+                currMoleTile.setIcon(moleIcon);
+            }
+        });
+
+        setPlantTimer = new Timer(1500, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //remove plant from current tile
+                if (currPlantTile != null) {
+                    currPlantTile.setIcon(null);
+                    currPlantTile = null;
+                }
+
+                //randomly select another tile
+                int num = random.nextInt(9); //0-8
+                JButton tile = board[num];
+
+                if (currMoleTile == tile) return;
+
+                //set tile to plant
+                currPlantTile = tile;
+                currPlantTile.setIcon(plantIcon);
+            }
+        });
+
+        setMoleTimer.start();
+        setPlantTimer.start();
+        frame.setVisible(true);
+    }
+}
---
+++ whac-a-mole-java-master/README.md
@@ -0,0 +1,13 @@
+# [Whac a Mole (Java)](https://youtu.be/rIQksHTwZzA)
+- Coding Tutorial: [https://youtu.be/ej8SatOj3V4](https://youtu.be/rIQksHTwZzA)
+
+In this tutorial, you will learn to create the whac a mole game with java using the built-in swing awt graphics library.
+
+While creating this mario themed whac-a-mole game, you will learn how to create a graphical user interface (GUI) using java's swing library, add click handlers to whack the mole, and randomly spawn the mole and piranha plant using the Timer and Random modules.
+
+[How to setup Java with Visual Studio Code](https://youtu.be/BB0gZFpukJU)
+
+![whac-a-mole-demo](https://github.com/ImKennyYip/whac-a-mole-java/assets/78777681/07ed1058-0ed6-4521-9272-5db68925f218)
+
+## Homework:
+You can continue working on this project if you like. One feature you can add is a reset button so that you can restart the game without having to rerun the program. As a follow up task, you can also add a variable to keep track of a high score. If you want a challenge, you can also add multiple piranha plants. You should store these piranha plants in an array. 
